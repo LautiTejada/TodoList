90df628fae5b3a585c53b40c2fce956aeb26bac5
MarcosDiM <marcosdimecom@gmail.com>
2025-04-02 21:57:27 -0300

    Logos agregados

--- todoList/src/components/layouts/Home/Home.tsx
+++ todoList/src/components/layouts/Home/Home.tsx
@@ -4,9 +4,18 @@ import styles from './Home.module.css';
 export const Home = () => {
     return (
     <div className={styles.navBacklog}>
-        <h2 className={styles.titleNav}>BackLog </h2>
+        <h2 className={styles.titleNav}>BackLog 
+        <span className="material-symbols-outlined">
+            import_contacts
+        </span>
+        </h2>
         <div className={styles.listSprints}>
-            <h3>Lista de Sprints</h3>
+            <h3>
+            <span className="material-symbols-outlined">
+                lists
+            </span>
+                Lista de Sprints
+            </h3>
             <div>
                 <CardSpring/>
             </div>
