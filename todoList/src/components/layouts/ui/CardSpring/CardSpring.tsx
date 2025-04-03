9a05c43db4e9a78432b2843f198d2b75fd3f22ed
MarcosDiM <marcosdimecom@gmail.com>
2025-04-03 12:22:51 -0300

    Card Sprint arreglada

--- todoList/src/components/layouts/ui/CardSpring/CardSpring.tsx
+++ todoList/src/components/layouts/ui/CardSpring/CardSpring.tsx
@@ -1,26 +1,59 @@
-import { Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import styles from "./CardSpring.module.css";
 export const CardSpring = () => {
   return (
     <>
       <div className={styles.cardSprintContainer}>
-        {["Light"].map((variant) => (
+        
           <Card
-            bg={variant.toLowerCase()}
-            key={variant}
-            text={variant.toLowerCase() === "light" ? "dark" : "white"}
-            style={{ width: "18rem" }}
+            style={{ width: "18rem", cursor:"pointer"}}
             className="mb-2"
           >
             <Card.Body>
-              <Card.Title>{variant} Card Title </Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+              <Card.Title className={styles.cardSprintTitle}> Sprint 1 </Card.Title>
+              <Card.Text className={styles.cardContainer}>
+                <p>Fecha de inicio: </p>
+                <p>Fecha de cierre: </p>
               </Card.Text>
+              <div className={styles.cardButtons}>
+                <Button
+                  className="d-flex align-items-center"
+                  variant="warning"
+                  >
+                  <span
+                      className="material-symbols-outlined"
+                      style={{ color: "black" }}
+                    >
+                    visibility
+                  </span>
+                </Button>
+                <Button
+                  className="d-flex align-items-center"
+                  variant="primary"
+                  
+                  >
+                    <span
+                        className="material-symbols-outlined"
+                        style={{ color: "black" }}
+                      >
+                      edit
+                    </span>
+                </Button>
+                <Button
+                  className="d-flex align-items-center"
+                  variant="danger"
+                  
+                  >
+                    <span
+                        className="material-symbols-outlined"
+                        style={{ color: "black" }}
+                      >
+                      delete
+                    </span>
+                </Button>
+              </div>
             </Card.Body>
           </Card>
-        ))}
       </div>
     </>
   );
