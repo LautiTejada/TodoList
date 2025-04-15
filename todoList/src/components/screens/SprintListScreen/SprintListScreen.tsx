0d56a88b61d921f73abb8cc02d99eddbe954b782
Marcos <marcosdimecom@gmail.com>
2025-04-14 22:53:02 -0300

    Modal agregar y ver sprint terminados

--- todoList/src/components/screens/SprintListScreen/SprintListScreen.tsx
+++ todoList/src/components/screens/SprintListScreen/SprintListScreen.tsx
@@ -1,10 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSprint } from "../../../hooks/useSprint";
 import { Header } from "../../layouts/Header/Header"
 import { CardSprint } from "../../layouts/ui/CardSprint/CardSprint";
 import { ISprint } from "../../../types/ITodo";
 import styles from "./SprintListScreen.module.css";
 import { Button } from "react-bootstrap";
+import { ModalAgregarSprint } from "../../modals/ModalAgregarSprint/ModalAgregarSprint";
 
 
 export const SprintListScreen = () => {
@@ -14,26 +15,37 @@ export const SprintListScreen = () => {
     useEffect(() => {
       getSprints(); 
     }, [getSprints]);
+
+
+  const [modalAgregarSprint, setModalAgregarSprint] = useState(false);
+    const handleModalAgregarSprint = () => {
+      setModalAgregarSprint(!modalAgregarSprint);
+    };
   
   return (
-    <div className={styles.sprintListScreenContainer}>
-      <Header/>
-      <div className={styles.titleSprintList}>
-        <h3>
-          Lista de sprints: 
-        </h3>
-        <div className={styles.addSprintButton}>
-          <Button variant="primary">
-            Agregar Sprint
-            <span className="material-symbols-outlined">add</span>
-          </Button>
+    <>
+      <div className={styles.sprintListScreenContainer}>
+        <Header/>
+        <div className={styles.titleSprintList}>
+          <h3>
+            Lista de sprints: 
+          </h3>
+          <div className={styles.addSprintButton}>
+            <Button variant="primary"
+            onClick={handleModalAgregarSprint}
+            >
+              Agregar Sprint
+              <span className="material-symbols-outlined">add</span>
+            </Button>
+          </div>
+        </div>
+        <div className={styles.mainSprintListScreen}>
+          {sprints.map((sprint : ISprint) => (
+              <CardSprint key={sprint.id} sprint={sprint}/>
+            ))}
         </div>
       </div>
-      <div className={styles.mainSprintListScreen}>
-         {sprints.map((sprint : ISprint) => (
-            <CardSprint key={sprint.id} sprint={sprint}/>
-          ))}
-      </div>
-    </div>
+      <ModalAgregarSprint show={modalAgregarSprint} handleClose={handleModalAgregarSprint}></ModalAgregarSprint>
+    </>
   )
 }
